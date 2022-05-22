f2a0eaa8b08f65d45ac29a1961ed2cc00e679249
Danila <de.levin.orp@gmail.com>
2022-05-22 21:02:34 +0300

    factorial whith recursion

---
+++ Example18/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+double Factorial (int n)
+{
+    if (n == 0) return 1;
+    else if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+
+for (int i = 1; i < 40; i++)
+{
+    WriteLine($"{i}! = {Factorial(i)}");
+}
