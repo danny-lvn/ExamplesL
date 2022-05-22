819bb167671159f1a20d8e333827e52170e08098
Danila <de.levin.orp@gmail.com>
2022-05-22 21:26:01 +0300

    fibonacci whith recursion

---
+++ Example19/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+/*
+f(1) = 1
+f(2) = 1
+f(n) = f(n-1) + f(n-2)
+*/
+
+double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 1; i < 10; i++)
+{
+    WriteLine($"Fib({i}) = {Fibonacci(i)}");
+}
