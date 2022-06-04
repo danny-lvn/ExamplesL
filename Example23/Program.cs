369520975e27ce4d75cbef2828cf301fcb36d936
Danila <de.levin.orp@gmail.com>
2022-06-04 18:19:08 +0300

    entry directory

---
+++ Example23/Program.cs
@@ -0,0 +1,14 @@
+﻿// Обход директория
+
+using System;
+using static System.Console;
+
+string path = "/Users/danila/Desktop/ExamplesL/Example002_HelloUser";
+DirectoryInfo di = new DirectoryInfo(path);
+WriteLine(di.CreationTime);
+FileInfo[] fi = di.GetFiles();
+
+for (int i = 0; i < fi.Length; i++)
+{
+    WriteLine(fi[i].Name);
+}
