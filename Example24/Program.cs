5b37da079573fa2fbebc5ee55d0b52fec3f17b08
Danila <de.levin.orp@gmail.com>
2022-06-04 18:40:24 +0300

    files and directories in directory

---
+++ Example24/Program.cs
@@ -0,0 +1,26 @@
+﻿// Метод для обхода директория
+
+using System;
+using static System.Console;
+
+Clear();
+
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + " ");
+    }
+    FileInfo[] files = catalog.GetFiles();
+    for (int i = 0; i < files.Length; i++)
+    {
+        WriteLine($"{indent}{files[i].Name}");
+    }
+}
+
+string path = "/Users/danila/Desktop/ExamplesL/Example002_HelloUser";
+CatalogInfo(path);
