d8674660822e93ddda40e891e86d13e53690afec
Danila <de.levin.orp@gmail.com>
2022-06-04 18:59:21 +0300

    tower game

---
+++ Example25/Program.cs
@@ -0,0 +1,13 @@
+﻿// Игра в башенки
+
+using System;
+using static System.Console;
+
+void Towers(string with = "1", string on = "3", string some = "2", int count = 3)
+{
+    if (count > 1) Towers(with, some, on, count -1);
+    WriteLine($"{with} >> {on}");
+    if (count > 1) Towers(some, on, with, count - 1);
+}
+
+Towers();
