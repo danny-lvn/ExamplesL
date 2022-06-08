11297fc56e93ea7a62b82e09458647fa146a9a27
Danila <de.levin.orp@gmail.com>
2022-06-08 12:25:20 +0300

    tower game v 1.1

--- Example25/Program.cs
+++ Example25/Program.cs
@@ -5,7 +5,7 @@ using static System.Console;
 
 void Towers(string with = "1", string on = "3", string some = "2", int count = 3)
 {
-    if (count > 1) Towers(with, some, on, count -1);
+    if (count > 1) Towers(with, some, on, count - 1);
     WriteLine($"{with} >> {on}");
     if (count > 1) Towers(some, on, with, count - 1);
 }
